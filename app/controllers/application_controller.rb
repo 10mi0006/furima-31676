f61d2b91eecf1dbedc2d271decd5e51273e3cb86
10mi0006 <mtktjd.tjtjmp@icloud.com>
2020-11-27 21:15:59 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up,
-        keys: [:nickname,  :birthday, :first_name,
+        keys: [:nickname,  :birthday, :first_name, :email, :password, :encrypted_password,
                :last_name, :first_name_kana, :last_name_kana,])
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,12 @@ class User < ApplicationRecord
           validates :birthday
           validates :email,    uniqueness: {case_sensitive: false},
                                format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-          validates :password, length: {minimum: 6},format: {/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
-          validates :encrypted_password, length: {minimum: 6},format: {/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
-      
+         
+
+          with_options format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}do
+          validates :password, length: {minimum: 6}
+         end
+
           with_options format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
             validates :first_name
             validates :last_name
