db08542eafa7d6e224c85e53b762974d4f87b7c2
10mi0006 <mtktjd.tjtjmp@icloud.com>
2020-12-09 19:11:05 +0900

    商品出品補足

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_fee_id, :shipment_address_id, :shipping_days_id,:price,:image,).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_fee_id, :shipment_address_id, :shipping_days_id,:price,:image).merge(user_id: current_user.id)
   end
 end
 
