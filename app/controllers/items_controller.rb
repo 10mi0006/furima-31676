5c3255b2fc96a92de4815d9804c8d96a6bedafc1
10mi0006 <mtktjd.tjtjmp@icloud.com>
2020-12-10 15:29:17 +0900

    商品詳細表示機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit,]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :move_to_root_path, except: [:index, :show, :search], unless: :user_signed_in?
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,12 +23,24 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
     @item = Item.find(params[:id])
-    
+    redirect_to root_path unless current_user == @item.user
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to items_path,notice: "変更しました" 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @items = Item.find(params[:id])
   end
 
   private
@@ -38,5 +51,11 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def move_to_root_path
+    redirect_to root_path 
+    flash[:alert] = "出品するには新規登録、ログインが必要です"
+  end
+
 end
 
