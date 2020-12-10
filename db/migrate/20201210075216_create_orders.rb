69bc5fb2e73c427b149865d0e5c9b048d105c3e1
10mi0006 <mtktjd.tjtjmp@icloud.com>
2020-12-10 19:12:09 +0900

    商品情報編集機能4

--- db/migrate/20201210075216_create_orders.rb
+++ db/migrate/20201210075216_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.integer :buy_id ,foreign_key: true
       t.integer :price  ,null: false
+      t.string  :postcode ,null:false
+      t.integer :prefecture_id ,null: false
+      t.string  :municipality ,null: false
+      t.string  :address ,null: false
+      t.string  :house_number ,null: false
+      t.string  :phone_number ,null: false
       t.timestamps
     end
   end
